135f606997a387d9c677cb2d2ad21062e3b1f4d4
JoshMBN <joshua.brennan.niu@gmail.com>
2022-05-10 13:59:50 +1200

    Added stuff

--- Program.cs
+++ Program.cs
@@ -9,10 +9,16 @@ namespace Studio1ClassQuiz
             // intro
             Console.WriteLine("Hello World!");
             // Question 1
-
+            task1();
             // Question 2
 
             // Question 3
+        }
+
+        public static void task1()
+        {
+
+
         }
     }
 }

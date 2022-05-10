11f1fb5a1afbf9bc9f1a40b93c93a837add710fe
sammy6345 <sammy6345@hotmail.com>
2022-05-10 13:52:52 +1200

    Added comments to prevent merge conflicts

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@ namespace Studio1ClassQuiz
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // intro
             Console.WriteLine("Hello World!");
+            // Question 1
+
+            // Question 2
+
+            // Question 3
         }
     }
 }

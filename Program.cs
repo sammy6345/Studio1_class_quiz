3455c736be435e16aa63240c7a0e1e34dd3085c8
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2022-05-10 14:15:06 +1200

    Added question

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Studio1ClassQuiz
             Console.WriteLine("Hello World!");
             // Question 1
             task1();
+            Q3();
             // Question 2
 
             // Question 3
@@ -19,6 +20,29 @@ namespace Studio1ClassQuiz
         {
 
 
+        }
+
+        public static void Q3()
+        {
+            string userInput;
+            Console.WriteLine();
+            Console.WriteLine("What is the hottest planet in the solar system?");
+            userInput = Console.ReadLine();
+            userInput.ToLower();
+
+            if (userInput == "venus" || userInput == "Venus")
+            {
+                Console.WriteLine();
+                Console.WriteLine("That is correct :^)");
+                Console.ReadKey();
+            }
+
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("That is incorrect");
+                Console.ReadKey();
+            }
         }
     }
 }
